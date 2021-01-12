afed796b6d56768353fca2bb975370834375bb69
Elliot <ebossi10@stuy.edu>
2021-01-12 17:54:56 -0500

    .

--- Radix.java
+++ Radix.java
@@ -1,6 +1,9 @@
 public class Radix{
 
   public static int nth(int n, int col){
+    double temp = Math.pow(10, col);
+    return Math.round((n/temp)%10);
+
   }
 
   public static int length(int n){
