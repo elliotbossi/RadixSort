ae6eb2db797fd1e94e00a9135d474bf258e12c85
Elliot <ebossi10@stuy.edu>
2021-01-12 18:11:00 -0500

    length

--- Radix.java
+++ Radix.java
@@ -2,11 +2,12 @@ public class Radix{
 
   public static int nth(int n, int col){
     double temp = Math.pow(10, col);
-    return Math.round((n/temp)%10);
-
+    return Math.abs(Math.round((n/temp)%10));
   }
 
   public static int length(int n){
+    n = Math.abs(n);
+    return (Integer.toString(n)).length();
   }
 
   public static void merge(MyLinkedList original, MyLinkedList[] buckets){
